1b0cb7a4abf33361811f500de6727dd44685f916
Heather <hnelson@hawk.iit.edu>
2018-04-28 15:07:58 -0500

    push

--- app/addtoproducts.php
+++ app/addtoproducts.php
@@ -14,15 +14,46 @@
 
 <body class="text-center">
 
+  <body class="text-center">
+    <!-- NAVBAR -->
     <div class="container">
-        <!--  Jumbotron is the header that you see, you can change its color-->
-        <div style="background:#02474d; color: #EEEEEE; !important" class="jumbotron">
-            <!-- Title  -->
-            <h1 class="display-3">CS 425 App</h1>
-            <!-- About Your App -->
-            <p class="lead">Small webapp for uploading products to a database.</p>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Welcome Back <?php echo $_SESSION['fname'];?></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="purchase.php">Purchase</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="addtoproducts.php">Add</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="logout.php">Logout</a>
+            </li>
+          </ul>
+          <!-- <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form> -->
         </div>
+      </nav>
     </div>
+    <div class="container">
+      <!--  Jumbotron is the header that you see, you can change its color-->
+      <div style="background:#02474d; color: #EEEEEE; !important; border-radius: 0px 0px 5px 5px;" class="jumbotron">
+        <!-- Title  -->
+        <h1 class="display-3">CS 425 App</h1>
+        <!-- About Your App -->
+        <p class="lead">Small webapp for uploading products to a database.</p>
+      </div>
+
+
     <div class="table-scrol">
         <h1 align="center">All Current Products</h1>
         <div class="table-responsive">
