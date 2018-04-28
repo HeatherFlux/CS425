74a8e50412769db1e198e42ae297027a7384d55c
Heather <hnelson@hawk.iit.edu>
2018-04-28 17:06:48 -0500

    highligh

--- app/addtoproducts.php
+++ app/addtoproducts.php
@@ -36,13 +36,13 @@ if(!$_SESSION['email'])
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-              <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+              <a class="nav-link" href="index.php">Home</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="purchase.php">Purchase</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="addtoproducts.php">Add</a>
+              <a class="nav-link" href="addtoproducts.php">Add<span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="logout.php">Logout</a>
