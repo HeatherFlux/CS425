3a2d7868315d24a575ee413b17b8c7df6bb77aa6
Heather <hnelson@hawk.iit.edu>
2018-04-28 17:07:52 -0500

    whoops

--- app/addtoproducts.php
+++ app/addtoproducts.php
@@ -35,14 +35,14 @@ if(!$_SESSION['email'])
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="index.php">Home</a>
-            </li>
             <li class="nav-item">
+              <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item active">
               <a class="nav-link" href="purchase.php">Purchase</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="addtoproducts.php">Add<span class="sr-only">(current)</span></a>
+              <a class="nav-link" href="addtoproducts.php">Add</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="logout.php">Logout</a>
