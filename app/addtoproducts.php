a72c14ea856501ff401f9b047c80c5d42ece2653
Heather <hnelson@hawk.iit.edu>
2018-04-28 15:08:13 -0500

    push

--- app/addtoproducts.php
+++ app/addtoproducts.php
@@ -37,10 +37,10 @@
               <a class="nav-link" href="logout.php">Logout</a>
             </li>
           </ul>
-          <!-- <form class="form-inline my-2 my-lg-0">
+          <form class="form-inline my-2 my-lg-0">
             <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          </form> -->
+          </form>
         </div>
       </nav>
     </div>
