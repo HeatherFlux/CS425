770dcaa378ba439d78a1e908d5a21c156e7b6428
Heather <hnelson@hawk.iit.edu>
2018-04-28 23:12:31 -0500

    purchase changes

--- app/purchase.php
+++ app/purchase.php
@@ -51,7 +51,7 @@ if(!$_SESSION['email'])
               <a class="nav-link" href="logout.php">Logout</a>
             </li>
           </ul>
-          <form class="form-inline my-2 my-lg-0">
+          <form class="form-inline my-2 my-lg-0" name="input" method="post" action="purchase.php">
             <input class="form-control mr-sm-2" type="search_product" name="p_name" placeholder="Search Product Name" aria-label="Search">
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
           </form> -->
