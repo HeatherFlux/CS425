073d93497a3de1730bf0a08b39e4a7978becc771
Heather <hnelson@hawk.iit.edu>
2018-04-30 11:30:05 -0500

    m

--- purchase.php
+++ purchase.php
@@ -1,9 +1,8 @@
-<?php
+﻿<?php
 session_start();
 
 if(!$_SESSION['email'])
 {
-
     header("Location: index.php");//redirect to login page to secure the welcome page without login access.
 }
 
@@ -67,11 +66,13 @@ if(!$_SESSION['email'])
         <!-- About Your App -->
         <p class="lead">Small webapp for uploading products to a database.</p>
       </div>
-      <div class="table-scrol">
+
+      <!-- the table starts here -->
+      <div class="table-scroll">
         <h1 align="center">All Current Products</h1>
         <div class="table-responsive">
-          <table class="table table-bordered table-hover table-striped" style="table-layout: fixed">
-            <thead>
+          <table class="table table-bordered table-hover table-striped">
+            <thead class="thead-dark">
               <tr>
                 <th>Product Id</th>
                 <th>Product Owner</th>
@@ -83,49 +84,87 @@ if(!$_SESSION['email'])
                 <th>Product Cost</th>
                 <th>Product Description</th>
                 <th>Product Image</th>
-                <th>Amount of Products Purchasing</th>
+                <th>Purchase Amount</th>
                 <th></th>
               </tr>
             </thead>
+            <tbody>
 
-      	<?php
-        include("db_connection.php");
+          <?php
+            include("db_connection.php");
 
-        //Search product name
-        //$search_product = $_POST['p_name'];
-        //$search_product = htmlspecialchars($search_product);
-        //$search_product = mysql_real_escape_string($search_product);
+            /*
+              Hey Laura, I fixed the code so that it passes the quantity and
+              product id of what was bought, and then update the quantity
+              number as well as the users wallet. Can you implement a
+              check for things like negative quantity etc etc.
+            */
 
-        // $test = mysqli_query($dbcon, "Select * from product where ('product_name' LIKE '%".$search_product."%'");
-        // echo $search_product;
-        echo " \n";
+            //update wallet
+            if (isset($_POST['quantity']))
+            {
+              $quant=$_POST['quantity'];
+              $pid=$_POST['productid'];
+              // echo 'Quant:'.$quant;
+              // echo ' Pid:'.$pid;
 
-        if (isset($_POST['p_name']))
-        {
-            //$view_selected_search="select * from product where product_name like '%".$search_product."%'";
-            $view_selected_search="select * from product where product_name like '%".$_POST['p_name']."%'";
-            //echo $view_selected_search; //testing
-        	  $run=mysqli_query($dbcon,$view_selected_search);
-      	}
-      	else
-      	{
-      		$view_products_query="select * from product"; //select query for viewing users.
-             echo $view_products_query; //testing
-      		$run=mysqli_query($dbcon,$view_products_query);//here run the sql query
-      	}
-        while($row=mysqli_fetch_array($run))//while look to fetch the result and store in a array $row.
-        {
-          $product_id=$row[0];
-          $product_owner=$row[1];
-          $product_name=$row[2];
-          $product_weight=$row[3];
-          $product_category=$row[4];
-          $product_quantity=$row[5];
-          $product_color=$row[6];
-          $product_cost=$row[7];
-          $product_description=$row[8];
-          $product_image_link=$row[9];
-      		?>
+              $wallet=$_SESSION['wallet'];
+              // get price and convert to int.
+              $get_price_query = "select product_cost from product where product_id like '%".$pid."%'";
+              $product_price = mysqli_query($dbcon, $get_price_query);//gets result
+              $row=mysqli_fetch_assoc($product_price); //converts result to int array
+              // echo ' product cost:'.$row["product_cost"]; // pulls int from array
+              $product_price=$row["product_cost"];
+
+              // get quant and convert
+              $get_quantity_query = "select product_quantity from product where product_id like '%".$pid."%'";
+              $product_quantity = mysqli_query($dbcon, $get_quantity_query);
+              $row=mysqli_fetch_assoc($product_quantity);
+              // echo ' product quant:'.$row["product_quantity"];
+              $product_quantity=$row["product_quantity"];
+
+              //update wallet
+              // echo ' wallet before:'.$wallet;
+              $wallet = $wallet - ($product_price * $quant); //update current wallet
+              // echo ' wallet after:'.$wallet;
+              $_SESSION['wallet'] = $_SESSION['wallet'] - ($product_price * $quant);
+
+              // update quant
+              // echo ' quant before:'.$product_quantity;
+              $product_quantity = $product_quantity-$quant;
+              // echo ' quant after:'.$product_quantity;
+              $update_quantity_query = "UPDATE product SET product_quantity=$product_quantity WHERE product_id like '%".$pid."%'";
+              mysqli_query($dbcon, $update_quantity_query);
+              header("Refresh:0"); // refreshes page to update wallet in the navbar
+            }
+
+            //Search product name
+            if (isset($_POST['p_name']))
+            {
+                //$view_selected_search="select * from product where product_name like '%".$search_product."%'";
+                $view_selected_search="select * from product where product_name like '%".$_POST['p_name']."%'";
+            	$run=mysqli_query($dbcon,$view_selected_search);
+          	}
+          	else
+          	{
+          		$view_products_query="select * from product"; //select query for viewing products.
+          		$run=mysqli_query($dbcon,$view_products_query);//here run the sql query
+          	}
+
+            while($row=mysqli_fetch_array($run))//while look to fetch the result and store in a array $row.
+            {
+              $product_id=$row[0];
+              $product_owner=$row[1];
+              $product_name=$row[2];
+              $product_weight=$row[3];
+              $product_category=$row[4];
+              $product_quantity=$row[5];
+              $product_color=$row[6];
+              $product_cost=$row[7];
+              $product_description=$row[8];
+              $product_image_link=$row[9];
+          ?>
+
           <tr>
             <!--here showing results in the table -->
       			<td><?php echo $product_id;  ?></td>
@@ -138,10 +177,19 @@ if(!$_SESSION['email'])
       			<td><?php echo $product_cost;  ?></td>
       			<td><?php echo $product_description;  ?></td>
       			<td><?php echo $product_image_link;  ?></td>
-      			<td><input class="form-control mr-sm-2" type="search" placeholder="Quantity" aria-label="Quantity"></td>
-      			<td><button class="btn btn-outline-success my-2 my-sm-0" type="submit">Purchase</button></a></td>
+      			<td>
+              <form class="form-inline my-2 my-lg-0" name="input" method="post" action="purchase.php">
+              <label for="quantity" class="sr-only">Quantity</label>
+              <input type="hidden" name="productid" type="int"  value="<?= $product_id ?>"/>
+              <input class="form-control mr-sm-2" type="int" name="quantity" placeholder="Quantity" aria-label="Quantity"/>
+            </td>
+            <td>
+              <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Purchase</button>
+              </form>
+            </td>
       		</tr>
-        <?php } ?>
+         <?php } ?>
+        </tbody>
       </table>
     </div>
   </body>
